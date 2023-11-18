3556caaf39ba9df453d58b417593a71b934f9e59
biodota1 <geonzonjamesmyerq@gmail.com>
2023-11-18 20:30:42 +0800

    Added route, middleware, config

---
+++ middleware/logger.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { format } = require("date-fns");
+const { v4: uuid } = require("uuid");
+const fs = require("fs");
+const fsPromises = require("fs").promises;
+const path = require("path");
+
+const logEvents = async (message, logFileName) => {
+  const createDate = `${format(new Date(), "yyyyMMdd\thh:mm:ss a")}`;
+  const createLogInfo = `${createDate}\t${uuid()}\t${message}\n`;
+
+  try {
+    if (!fs.existsSync(path.join(__dirname, "..", "logs"))) {
+      await fsPromises.mkdir(path.join(__dirname, "..", "logs"));
+    }
+    await fsPromises.appendFile(
+      path.join(__dirname, "..", "logs", logFileName),
+      createLogInfo
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const logger = (req, res, next) => {
+  logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, "reqLog.log");
+  console.log(`${req.method}\t${req.path}`);
+  next();
+};
+
+module.exports = { logEvents, logger };
